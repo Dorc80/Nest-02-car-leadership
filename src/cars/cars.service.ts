b3d600f0a3541a119df55e8b2ddc8a1ec105beb8
drojas10 <drojas10@bancoestado.cl>
2023-09-23 13:23:43 -0300

    finaliza 02-car-leadership

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -7,21 +7,21 @@ import { CreateCarDto, UpdateCarDto } from './dto';
 export class CarsService {
 
     private cars: Car[] = [
-        {
-            id: uuid(),
-            brand: 'Toyota',
-            model: 'Corolla'
-        },
-        {
-            id: uuid(),
-            brand: 'Honda',
-            model: 'Civic'
-        },
-        {
-            id: uuid(),
-            brand: 'Jeep',
-            model: 'Cherokee'
-        },
+        // {
+        //     id: uuid(),
+        //     brand: 'Toyota',
+        //     model: 'Corolla'
+        // },
+        // {
+        //     id: uuid(),
+        //     brand: 'Honda',
+        //     model: 'Civic'
+        // },
+        // {
+        //     id: uuid(),
+        //     brand: 'Jeep',
+        //     model: 'Cherokee'
+        // },
     ];
 
     findAll() {
@@ -72,5 +72,8 @@ export class CarsService {
         this.cars = this.cars.filter(car => car.id !== id);
     }
 
+    fillCarsWithSeedData(cars: Car[]) {
+        this.cars = cars;
+    }
 
 }
